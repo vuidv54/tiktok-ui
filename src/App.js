64f6bad0c61469e1e64f2b27d89b634f2c26e878
doanvanvui0504 <vanvuidoan1@gmail.com>
2022-07-21 09:11:37 +0700

    Dựng base dự án Tiktok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <h1>Xin chao moi nguoi</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Xin chao moi nguoi</h1>
+        </div>
+    );
 }
 
 export default App;
