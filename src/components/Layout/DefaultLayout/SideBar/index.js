731f5319976c1a85199b7a659da7ec4f4edc2242
doanvanvui0504 <vanvuidoan1@gmail.com>
2022-07-21 22:35:49 +0700

    Dựng khung cho Default Layout

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './SideBar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function SideBar() {
-    return <h1>SideBar</h1>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h1>SideBar</h1>
+        </aside>
+    );
 }
 
 export default SideBar;
