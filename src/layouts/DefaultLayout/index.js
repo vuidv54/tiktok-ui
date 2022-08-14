6adf0949613d770effc847bf357ba70998459765
doanvanvui0504 <vanvuidoan1@gmail.com>
2022-08-14 17:54:59 +0700

    Tái cấu trúc và tối ưu code #1

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
 import SideBar from './SideBar';
-import Header from '~/components/Layout/components/Header';
+import Header from '~/layouts/components/Header';
 
 const cx = classNames.bind(styles);
 
--- src/layouts/components/Header/Search/index.js
+++ src/layouts/components/Header/Search/index.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-import * as searchServices from '~/apiServices/searchServices';
+import * as searchService from '~/services/searchService';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
@@ -32,7 +32,7 @@ function Search() {
         const fetchApi = async () => {
             setLoading(true);
 
-            const result = await searchServices.search(debounced);
+            const result = await searchService.search(debounced);
 
             setSearchResult(result);
 
